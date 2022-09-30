55aa590f3796629b6a2145b27a3bef39215ccd8d
wayne james <jackmcwilliams@Jacks-Air.lan>
2022-09-29 19:53:31 -0700

    input if statements

--- script.js
+++ script.js
@@ -18,10 +18,32 @@ function generatePassword() {
   var special = confirm("Include special characters in your password?")
 
   var numList = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",]
-  var symList = 
-}
+  var symList = [ "!", "@", "#", "$", "%", "^", "&", "*"]
+  var lCaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
+    "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  var uCaseList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", 
+  "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+ 
+  var output = []
 
-// Write password to the #password input
+  if(numbers === true) (
+    output.push(numList)
+  )
+
+  if(uCase === true) (
+    output.push(uCaseList)
+
+  )
+
+  if(lCase === true) (
+    output.push(lCaseList)
+  )
+
+    if(special === true) (
+      output.push(symList)
+    )
+  console.log(output)
+}
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
